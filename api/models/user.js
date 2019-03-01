21b2b170db11caf81bd27150772d597f2a81a69b
YMiloux <yann.miloux@universalmedica.com>
2019-03-01 16:46:26 +0100

    Global \\ Init project

---
+++ api/models/user.js
@@ -0,0 +1,40 @@
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+var userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    has: String,
+    salt: String
+});
+
+// SET PASSWORD WITH HASH & SALT
+userSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+};
+
+// VALID PASSWORD WITH HASH & SALT
+userSchema.methods.validPassword = function(password){
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+    return this.hash === hash;
+};
+
+userSchema.methods.generateJWT = function(){
+    var expire = new Date();
+    expire.setDate(expire.getDate() + 7);
+
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        name: this.name,
+        exp: parseInt(expire.getTime() / 1000)
+    }, process.env.APP_SECRET);
+};
